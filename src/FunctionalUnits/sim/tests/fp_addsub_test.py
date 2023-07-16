7949fd1e4103d977e70c83ffddf3719dd9c2c4ab
Courtney McBeth <cmcbeth5287@gmail.com>
2023-07-16 10:38:20 -0400

    Converting floating points to hex

--- src/FunctionalUnits/sim/tests/fp_addsub_test.py
+++ src/FunctionalUnits/sim/tests/fp_addsub_test.py
@@ -2,32 +2,42 @@
 # Floating Point Add/Subtract Test
 #===============================================================================
 
+import struct
 from pymtl3 import *
 from pymtl3.stdlib.test_utils import run_test_vector_sim
 from sim.fp_addsub import fp_addsub
+
+def float_list_to_hex(float_list):
+    """
+    Converts a list of floating-point values to their hexadecimal representation (single-precision).
+    """
+    hex_list = [hex(struct.unpack('!I', struct.pack('!f', value))[0]) for value in float_list]
+    return hex_list
 
 def test_addition(cmdline_opts):
     addsub = fp_addsub(WIDTH=32)
     test_cases = [
         # Input1, Input2, Operation Type, Expected Result
-        ('a b subtract y*'),
-        [1.5, 2.5, 0, 4.0],
-        [0.0, 0.0, 0, 0.0],
-        [-3.2, 1.7, 0, -1.5],
+        [1.5, 2.5, 0.0, 4.0],
+        [0.0, 0.0, 0.0, 0.0],
+        [-3.2, 1.7, 0.0, -1.5],
         [100.123, -50.321, 0, 49.802],
         [10.0, 0.0, 0, 10.0],
     ]
+    test_cases = [float_list_to_hex(test) for test in test_cases]
+    test_cases.insert(0, ('a b subtract y*'))
     run_test_vector_sim(addsub, test_cases, cmdline_opts)
 
 def test_subtraction(cmdline_opts):
     addsub = fp_addsub(WIDTH=32)
     test_cases = [
         # Input1, Input2, Operation Type, Expected Result
-        ('a b subtract y*'),
         [5.0, 2.0, 1, 3.0],
         [0.0, 0.0, 1, 0.0],
         [-3.2, -1.7, 1, -1.5],
         [100.123, 50.321, 1, 49.802],
         [10.0, 10.0, 1, 0.0],
     ]
+    test_cases = [float_list_to_hex(test) for test in test_cases]
+    test_cases.insert(0, ('a b subtract y*'))
     run_test_vector_sim(addsub, test_cases, cmdline_opts)

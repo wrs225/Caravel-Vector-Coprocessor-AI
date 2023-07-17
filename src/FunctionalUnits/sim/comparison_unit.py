04f7fae054770d8bba0eac9d5e760ecabbfcd4ed
Courtney McBeth <cmcbeth5287@gmail.com>
2023-07-17 18:07:00 -0500

    Adding tests for comparison unit

---
+++ src/FunctionalUnits/sim/comparison_unit.py
@@ -0,0 +1,14 @@
+from pymtl3 import *
+from pymtl3.passes.backends.verilog import *
+
+# PyMTL wrapper for the Comparison Unit Verilog model
+
+class comparison_unit(VerilogPlaceholder, Component):
+
+    def construct(self, WIDTH=32):
+        self.set_metadata(VerilogTranslationPass.explicit_module_name, 'ComparisonUnitWrapper')
+
+        self.in1 = InPort(mk_bits(WIDTH))
+        self.in2 = InPort(mk_bits(WIDTH))
+        self.control = InPort(mk_bits(2))
+        self.predicate = OutPort(mk_bits(1))

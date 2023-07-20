092abd1a0da9b9a4092ced70497647f4525628bb
Courtney McBeth <cmcbeth5287@gmail.com>
2023-07-20 18:50:02 -0500

    Working on random fp multiplier tests, might be issues with overflow

--- src/FunctionalUnits/sim/block_test/fp_multiplier_test.py
+++ src/FunctionalUnits/sim/block_test/fp_multiplier_test.py
@@ -4,9 +4,9 @@
 
 import random
 from pymtl3 import *
-from pymtl3.stdlib.test_utils import run_test_vector_sim
+from pymtl3.stdlib.test_utils import run_test_vector_sim, config_model_with_cmdline_opts
 from sim.fp_multiplier import fp_multiplier
-from sim.block_test.utils import float_to_hex
+from sim.block_test.utils import float_to_hex, ieee_754_to_float
 
 def test_small(cmdline_opts):
     multiplier = fp_multiplier()
@@ -65,12 +65,26 @@ def test_extremes(cmdline_opts):
 
 def test_random(cmdline_opts):
     multiplier = fp_multiplier()
-    test_vectors = [('a', 'b', 'y*')]
+    multiplier = config_model_with_cmdline_opts(multiplier, cmdline_opts, duts=[])
+    multiplier.apply(DefaultPassGroup())
+
+    def tr(dut, a, b):
+        # Write input value to input port
+        dut.a @= a
+        dut.b @= b
+        
+        retval = dut.y
+        dut.sim_tick()
+        return retval
 
     for _ in range(1000):
         a = random.uniform(-1e38, 1e38)
         b = random.uniform(-1e38, 1e38)
-        expected_result = a * b
-        test_vectors.append([float_to_hex(a), float_to_hex(b), float_to_hex(expected_result)])
+        actual_result = tr(multiplier, float_to_hex(a), float_to_hex(b))
 
-    run_test_vector_sim(multiplier, test_vectors, cmdline_opts)
+        bit_expected = float_to_hex(a * b)
+        clipped_expected = ieee_754_to_float(bit_expected)
+
+        # Account for inf and nan, then check floats within tolerance
+        assert (bit_expected == int(actual_result)) or \
+            abs(clipped_expected - ieee_754_to_float(actual_result)) < 1e-7
--- src/FunctionalUnits/sim/block_test/utils.py
+++ src/FunctionalUnits/sim/block_test/utils.py
@@ -33,3 +33,46 @@ def float_to_hex(f):
 # print(float_to_hex(1e-40))  # Output: 0
 # print(float_to_hex(-3.5e38))  # Output: 4286578688
 # print(float_to_hex(-1e-40))  # Output: 0
+
+def ieee_754_to_float(int_val):
+    # Construct the binary format string
+    binary_format = ''.join(bin(int_val)[2:].zfill(32))
+
+    # Extract sign, exponent, and mantissa from binary string
+    sign = int(binary_format[0], 2)
+    exponent = int(binary_format[1:9], 2)
+    mantissa = int(binary_format[9:], 2)
+
+    print(sign)
+    print(exponent)
+    print(mantissa)
+
+    # If exponent is all 1's, it's either an infinity or a NaN
+    if exponent == 255:
+        if mantissa == 0:
+            return float('inf') if sign == 0 else float('-inf')
+        else:
+            return float('nan')
+
+    # If exponent and mantissa are all 0's, it's zero
+    elif exponent == 0 and mantissa == 0:
+        return 0.0
+
+    # Compute the actual exponent value
+    exponent = exponent - 127
+
+    print(exponent)
+
+    # Compute the actual mantissa value
+    mantissa = 1 + mantissa / 2**23
+
+    # Compute the final floating point value
+    float_val = (-1)**sign * 2**exponent * mantissa
+
+    return float_val
+
+# Example: converting a 32-bit single precision IEEE 754 floating point number to a float
+# int_val = 1065353216  # binary: 0b00111111100000000000000000000000, represents 1.0 in floating point
+# float_val = ieee_754_to_float(int_val)
+# print(float_val)  # Output: 1.0
+# print(ieee_754_to_float(0x3d1a52aa))

95c6c9a688261d6f70420bd11dfb129a64795edf
Courtney McBeth <cmcbeth5287@gmail.com>
2023-08-01 21:17:41 -0500

    Queue passing initial tests - need to add more

--- src/Dpath/sim/queue.v
+++ src/Dpath/sim/queue.v
@@ -16,12 +16,14 @@ module queue #(parameter WIDTH = 8, DEPTH = 16) (
     typedef logic [WIDTH-1:0] data_t;
     data_t queue [0:DEPTH-1];
     logic [$clog2(DEPTH)-1:0] head, tail;
+    logic [$clog2(DEPTH)-1:0] count;
     logic full, empty;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
             head <= 0;
             tail <= 0;
+            count <= 0;
             full <= 0;
             empty <= 1;
         end else begin
@@ -30,23 +32,21 @@ module queue #(parameter WIDTH = 8, DEPTH = 16) (
                 tail <= tail + 1;
                 if (tail == DEPTH)
                     tail <= 0;
-                if (tail == head)
-                    full <= 1;
-                empty <= 0;
+                count <= count + 1;
             end
             if (send_rdy && send_val && !empty) begin
                 head <= head + 1;
                 if (head == DEPTH)
                     head <= 0;
-                if (head == tail)
-                    empty <= 1;
-                full <= 0;
+                count <= count - 1;
             end
+            full <= (count == DEPTH);
+            empty <= (count == 0);
         end
     end
 
     assign recv_rdy = !full;
     assign send_val = !empty;
-    assign send_msg = queue[head]; // The data is read in the combinational part, which removes the delay
+    assign send_msg = queue[head];
 
 endmodule

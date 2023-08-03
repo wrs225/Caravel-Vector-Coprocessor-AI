42000bc49e30f1beaadb5a172c95487ca88c2bfd
Courtney McBeth <cmcbeth5287@gmail.com>
2023-08-03 12:43:34 -0500

    AAAAAAAAAAAAHHHHH

--- src/Dpath/sim/queue.v
+++ src/Dpath/sim/queue.v
@@ -15,18 +15,18 @@ module queue #(parameter WIDTH = 8, DEPTH = 16) (
 
     typedef logic [WIDTH-1:0] data_t;
     data_t queue [0:DEPTH-1];
-    logic [$clog2(DEPTH)-1:0] head, tail, next_head, next_tail;
+    logic [$clog2(DEPTH)-1:0] head, tail;
     logic full, empty, next_full, next_empty;
 
-    always_ff @(posedge clk) begin
+    always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
             head <= 0;
             tail <= 0;
             full <= 0;
             empty <= 1;
         end else begin
-            head <= next_head;
-            tail <= next_tail;
+            head <= (send_rdy && send_val && !empty) ? (head == DEPTH-1 ? 0 : head + 1) : head;
+            tail <= (recv_rdy && recv_val && !full) ? (tail == DEPTH-1 ? 0 : tail + 1) : tail;
             full <= next_full;
             empty <= next_empty;
         end
@@ -39,24 +39,18 @@ module queue #(parameter WIDTH = 8, DEPTH = 16) (
     end
 
     always_comb begin
-        next_head = head;
-        next_tail = tail;
         next_full = full;
         next_empty = empty;
 
-        // Receive logic
         if (recv_rdy && recv_val && !full) begin
-            next_tail = next_tail + 1;
-            if (next_tail == DEPTH) next_tail = 0;
-            if (next_tail == head) next_full = 1;
-            next_empty = 0;
-        end
-
-        // Send logic
-        if (send_rdy && send_val && !empty) begin
-            next_head = next_head + 1;
-            if (next_head == DEPTH) next_head = 0;
-            if (next_head == tail) next_empty = 1;
+            if (send_rdy && send_val && !empty) begin
+                // If sending and receiving simultaneously, the full and empty state remain the same
+            end else begin
+                next_full = (tail == (DEPTH-1) ? 0 : tail + 1) == head;
+                next_empty = 0;
+            end
+        end else if (send_rdy && send_val && !empty) begin
+            next_empty = (head == (DEPTH-1) ? 0 : head + 1) == tail;
             next_full = 0;
         end
     end
